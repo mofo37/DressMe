dd38373d39f391a134439ace25ccf5a24ba998d6
Morgan Fogarty <mofo37@gmail.com>
2018-12-17 13:19:37 -0800

    Add login/logout sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'greetings#welcome'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   resources :users
   resources :photos
   resources :pieces
   resources :outfits
+  resources :sessions
 end
