a9e07c4faa4c3a3ac2d7eeeedd3bb32c57751cb1
Morgan Fogarty <mofo37@gmail.com>
2018-12-18 12:17:13 -0800

    Add username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
 
   has_many :outfits
   has_many :pieces
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_223102) do
+ActiveRecord::Schema.define(version: 2018_12_18_200326) do
 
   create_table "outfits", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_12_17_223102) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
   end
 
 end
