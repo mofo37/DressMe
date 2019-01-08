d1396f4ee7c1a3c9e293c1fdc8039e58d8072411
Morgan Fogarty <mofo37@gmail.com>
2019-01-07 19:20:27 -0800

    Make uploaded images of pieces display on Photos index view

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,13 @@
 class PhotosController < ApplicationController
 
   def index
-    @collections = [current_user.pieces].flatten
-    # , current_user.outfits
-    # @photos = collections.map {|o| o.images }
-
+    collections = [current_user.pieces].flatten
+    collections = collections.map { |o| o.images if o.images.attached? }.compact
+    @photos = []
+    collections.each do |collection|
+      collection.each do |attachment|
+        @photos << attachment
+      end
+    end
   end
 end
