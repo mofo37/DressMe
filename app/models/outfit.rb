5c68f4b700c9d934e2d62c39aadf39b5d8c03cd7
Morgan Fogarty <mofo37@gmail.com>
2019-01-10 10:43:27 -0800

    When Outfit or Piece deleted, Stylings will be deleted, too

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,7 +1,7 @@
 class Outfit < ApplicationRecord
   belongs_to :user, optional: true
 
-  has_many :stylings
+  has_many :stylings, dependent: :destroy
   has_many :pieces, through: :stylings
 
   has_many_attached :images
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,7 @@
 class Piece < ApplicationRecord
   belongs_to :user, optional: true
 
-  has_many :stylings
+  has_many :stylings, dependent: :destroy
   has_many :outfits, through: :stylings
 
   has_many_attached :images
