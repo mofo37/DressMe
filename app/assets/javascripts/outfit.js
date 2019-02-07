ca254b3e083847b309bff9d09f6690596084299f
Morgan Fogarty <mofo37@gmail.com>
2019-02-06 19:46:21 -0800

    Don't use ES6

--- app/assets/javascripts/outfit.js
+++ app/assets/javascripts/outfit.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $("#outfit_formality").on('input change',function(){
-        const value = $("#outfit_formality").val()
-        $(".text").text(value)
-    });    
+$(document).ready(function () {
+  $("#outfit_formality").on('input change', function () {
+    var value = $("#outfit_formality").val()
+    $(".text").text(value)
+  });
 });
 
-$(document).ready(function() {
-    $(".pieces").hide();
-        $(".show-pieces").click(function(e) {
-            e.preventDefault();
-            $(".pieces").toggle();
-    });
+$(document).ready(function () {
+  $(".pieces").hide();
+  $(".show-pieces").click(function (e) {
+    e.preventDefault();
+    $(".pieces").toggle();
+  });
 });
