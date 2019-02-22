1d26594e6a2ca4821d37068c812ff74178d80794
Morgan Fogarty <mofo37@gmail.com>
2019-02-21 20:16:14 -0800

    Add comments to js

--- app/assets/javascripts/outfit.js
+++ app/assets/javascripts/outfit.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
+  // Syncs the outfit formality slider on the outfit form with the text value below it. 
   $("#outfit_formality").on('input change', function () {
     var slider_value = $("#outfit_formality").val()
     $(".text").text(slider_value)
   });
-});
-
-$(document).ready(function () {
+  
+  // Hides and shows pieces in the outfit form.
   $(".pieces").hide();
   $(".pieces-button").click(function (e) {
     e.preventDefault();
