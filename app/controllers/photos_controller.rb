803431f684f77a32f019124a8edccd694e54026f
Morgan Fogarty <mofo37@gmail.com>
2019-03-09 18:33:50 -0800

    In closet, click a photo => outfit/piece of that photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,14 +2,15 @@ class PhotosController < ApplicationController
 
   def index
     if current_user
-      collections = [current_user.pieces, current_user.outfits].flatten
-      collections = collections.map { |o| o.images if o.images.attached? }.compact
-      @photos = []
-      collections.each do |collection|
-        collection.each do |attachment|
-          @photos << attachment
-        end
-      end
+      @photos = [current_user.pieces, current_user.outfits].flatten
     end
   end
 end
+
+      # collections = collections.map { |o| o.images if o.images.attached? }.compact
+      # @photos = []
+      # collections.each do |collection|
+      #   collection.each do |attachment|
+      #     @photos << attachment
+      #   end
+      # end
