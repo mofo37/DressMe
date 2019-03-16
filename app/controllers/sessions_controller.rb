d258e23658acfe2fae7b492151daf0573ec5dfe1
Shane Becker <veganstraightedge@gmail.com>
2019-03-16 14:24:10 -0700

    Disallow /signin for signed in users
    Co-authored-by: Morgan Fogarty <mofo37@gmail.com>
    Co-authored-by: Shane Becker <veganstraightedge@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to root_path if current_user
   end
 
   def create
     user = User.find_by(email: params[:email])
-    
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
