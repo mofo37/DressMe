ded6a38b537f6483bde9bdefb6d3391af338ec5d
Shane Becker <veganstraightedge@gmail.com>
2019-03-16 14:39:54 -0700

    Delete unused (for now) /test folder
    Co-authored-by: Morgan Fogarty <mofo37@gmail.com>
    Co-authored-by: Shane Becker <veganstraightedge@gmail.com>

--- test/controllers/greetings_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get greetings_welcome_url
-    assert_response :success
-  end
-
-end
