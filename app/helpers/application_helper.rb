a11b12ee7278f76e1943722e9b3e36219272552a
Morgan Fogarty <mofo37@gmail.com>
2019-04-10 18:20:10 -0700

    Add styling to sign in/ sign up.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sign_in_or_sign_up
+    if on_sign_in_page?
+      'Sign in'
+    else
+      'Sign up'
+    end 
+  end
+
+  def on_sign_in_page?
+    controller_name == 'sessions'
+  end
 end
